6fd999b4c1629e987ece961bb8e6420a5ccb6a5d
jkahn <jkahn@amazon.com>
2017-02-18 13:42:23 -0600

    Remove deploy.sh

--- server/deploy.sh
+++
@@ -1,7 +0,0 @@
-echo '*** Package ***'
-aws cloudformation package --template-file app-sam.yaml --s3-bucket public-bikes-dev --output-template-file app-sam-output.yaml
-
-echo '*** Deploy ***'
-aws cloudformation deploy --template-file app-sam-output.yaml --stack-name public-bikes-dev --capabilities CAPABILITY_IAM
-
-echo '*** FINISHED ***'
